2fb07933b92d032d2b8b84007e4e638e7303d30f
balajimurugesan2022 <balajivengatesh@zumen.com>
2022-12-09 15:47:21 +0530

    Added jars

--- makethepartDBCOnfigScripts.js
+++ makethepartDBCOnfigScripts.js
@@ -1,3 +1,3 @@
 var db = connect('172.31.58.231:8560/makethepart')
 db.auth({user:"USER",pwd:"PWD"})
-db.orgFunctionality.insert({orgId:"buy2324950f126b",functionality:"PRICOLPARTSUPLOAD"});
+db.OrgFunctionality.insert({orgId:"buy2324950f126b",functionality:"PRICOLPARTSUPLOAD"});
