c37ebafa833ff03bbe245731a314a07de8755c93
balajimurugesan2022 <105208108+balajimurugesan2022@users.noreply.github.com>
2023-03-08 18:11:10 +0530

    Update makethepartDBCOnfigScripts.js
    Added ALT_REF_PART_COST functionality

--- makethepartDBCOnfigScripts.js
+++ makethepartDBCOnfigScripts.js
@@ -4,4 +4,5 @@ var db = connect('localhost/makethepart')
 db.auth({ user: "USERNAME", pwd: "PASSWORD" })
 db.OrgFunctionality.drop()
 db.OrgFunctionality.insert({orgId:"buy4adad087f601",functionality:"PRICOLPARTSUPLOAD"});
+db.orgFunctionality.insertOne({orgId: "buy2324950f126b",functionality:"ALT_REF_PART_COST"});
 db.OrgFunctionality.insert({orgId:"buy4adad087f601",functionality:"PRICOLQUOTESEND"});
