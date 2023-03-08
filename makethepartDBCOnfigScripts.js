5a5ba7d47affa48a852859818486665000a7c95c
Sanjai Kumar A <89142406+sanjai-zumen@users.noreply.github.com>
2023-03-08 19:28:26 +0530

    Update makethepartDBCOnfigScripts.js

--- makethepartDBCOnfigScripts.js
+++ makethepartDBCOnfigScripts.js
@@ -2,7 +2,7 @@
 // mongo MONGOURL zumenagent.js
 var db = connect('localhost/makethepart')
 db.auth({ user: "USERNAME", pwd: "PASSWORD" })
-db.OrgFunctionality.drop()
-db.OrgFunctionality.insert({orgId:"buy4adad087f601",functionality:"PRICOLPARTSUPLOAD"});
+db.orgFunctionality.drop()
+db.orgFunctionality.insert({orgId:"buy4adad087f601",functionality:"PRICOLPARTSUPLOAD"});
 db.orgFunctionality.insertOne({orgId: "buy2324950f126b",functionality:"ALT_REF_PART_COST"});
-db.OrgFunctionality.insert({orgId:"buy4adad087f601",functionality:"PRICOLQUOTESEND"});
+db.orgFunctionality.insert({orgId:"buy4adad087f601",functionality:"PRICOLQUOTESEND"});
