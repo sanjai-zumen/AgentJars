0855ec03f5ee18074f1edbe2a721763fa92351d1
Sanjai Kumar A <89142406+sanjai-zumen@users.noreply.github.com>
2023-03-16 16:45:25 +0530

    Update makethepartDBCOnfigScripts.js

--- makethepartDBCOnfigScripts.js
+++ makethepartDBCOnfigScripts.js
@@ -4,6 +4,6 @@ var db = connect('localhost/makethepart')
 db.auth({ user: "USERNAME", pwd: "PASSWORD" })
 db.orgFunctionality.drop()
 db.orgFunctionality.insert({orgId:"buy4adad087f601",functionality:"PRICOLPARTSUPLOAD"});
-db.orgFunctionality.insertOne({orgId: "buy2324950f126b",functionality:"ALT_REF_PART_COST"});
+db.orgFunctionality.insertOne({orgId: "buy4adad087f601",functionality:"ALT_REF_PART_COST"});
 db.orgFunctionality.insert({orgId:"buy4adad087f601",functionality:"PRICOLQUOTESEND"});
 db.orgFunctionality.insertOne({functionality: 'ECC_PO_FETCH', orgId: 'buy4adad087f601'});
